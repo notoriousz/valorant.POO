ab7d77166f4542153e0e38b2bb69f25fc1a9bb73
Matheus Serafim <mahserafa2016@gmail.com>
2022-04-25 16:37:28 -0300

    entry point companion object

---
+++ src/main/kotlin/com/valorant/Application.kt
@@ -0,0 +1,11 @@
+package com.valorant
+
+class Application {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
