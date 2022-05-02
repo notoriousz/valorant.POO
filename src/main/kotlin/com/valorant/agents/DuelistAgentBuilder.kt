8348cc7276d803962c94b98ffba373ce61a1a5c1
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-01 23:14:23 -0300

    duelist agent class

--- src/main/kotlin/com/valorant/agents/DuelistAgentBuilder.kt
+++ src/main/kotlin/com/valorant/agents/DuelistAgentBuilder.kt
@@ -1,4 +1,34 @@
 package com.valorant.agents
 
-class DuelistAgentBuilder {
+import com.valorant.arsenal.Arsenal
+import java.time.LocalDateTime
+
+class DuelistAgentBuilder(
+    override val nameOfAgent: String,
+    override val agentRole: String,
+    override val createdAt: LocalDateTime = LocalDateTime.now(),
+    override val biography: String,
+    override var currentGun: Arsenal
+) : Agent {
+
+    // default properties
+    private val agentHeight: Double = Agent.height
+    private val agentHealth: Double = Agent.health
+    private val numberOfSkills: Int = Agent.numberOfSkills
+
+    override fun executeSkillOne(): String {
+        return "Devour"
+    }
+
+    override fun executeSkillTwo(): String {
+        return "Dismiss"
+    }
+
+    override fun executeSkillThree(): String {
+        return "Ravenous Gaze"
+    }
+
+    override fun executeSkillUltimate(): String {
+        return "Empress"
+    }
 }
