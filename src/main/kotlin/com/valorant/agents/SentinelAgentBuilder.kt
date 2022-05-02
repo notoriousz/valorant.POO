045f4ce952a2613bbe459b098bc9b51bb0e1f03a
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-01 23:19:46 -0300

    sentinel agent class

--- src/main/kotlin/com/valorant/agents/SentinelAgentBuilder.kt
+++ src/main/kotlin/com/valorant/agents/SentinelAgentBuilder.kt
@@ -1,4 +1,34 @@
 package com.valorant.agents
 
-class SentinelAgentBuilder {
+import com.valorant.arsenal.Arsenal
+import java.time.LocalDateTime
+
+class SentinelAgentBuilder(
+    override val nameOfAgent: String,
+    override val agentRole: String,
+    override val createdAt: LocalDateTime,
+    override val biography: String,
+    override var currentGun: Arsenal
+) : Agent {
+
+    // default properties
+    private val agentHeight: Double = Agent.height
+    private val agentHealth: Double = Agent.health
+    private val numberOfSkills: Int = Agent.numberOfSkills
+
+    override fun executeSkillOne(): String {
+        return "Cyber Cage"
+    }
+
+    override fun executeSkillTwo(): String {
+        return "Surveillance Camera"
+    }
+
+    override fun executeSkillThree(): String {
+        return "Wire Trap"
+    }
+
+    override fun executeSkillUltimate(): String {
+        return "Neural Assault"
+    }
 }
