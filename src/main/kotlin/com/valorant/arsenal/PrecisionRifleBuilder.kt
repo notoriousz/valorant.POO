9f4d7d18e896cc45fee3facf97d8ac7f8fa0a3fb
Matheus Serafim <mahserafa2016@gmail.com>
2022-05-02 22:53:15 -0300

    add precision rifle

--- src/main/kotlin/com/valorant/arsenal/PrecisionRifleBuilder.kt
+++ src/main/kotlin/com/valorant/arsenal/PrecisionRifleBuilder.kt
@@ -1,19 +1,19 @@
 package com.valorant.arsenal
 
-class RifleBuilder(
+class PrecisionRifleBuilder(
     override val name: String,
     override val description: String
 ) : Arsenal {
 
-    override val numberOfProjectiles: Int = 24
+    override val numberOfProjectiles: Int = 12
 
-    override val damage: Int = 100
+    override val damage: Int = 300
 
     override val weight: String = "heavy"
 
-    override val recoil: Double = 0.9
+    override val recoil: Double = 1.0
 
-    override val size: Double = 99.0
+    override val size: Double = 100.0
 
     override val precision: Double = 1.0
 
